2d1c8401eba747606470663a4d4e4f01c43fbafe
nikeshprajapati <nikeshprajapati7@gmail.com>
2020-02-14 11:14:06 -0600

    Copied

--- app/src/main/java/com/e/bloodme/MainActivity.java
+++ app/src/main/java/com/e/bloodme/MainActivity.java
@@ -3,12 +3,25 @@ package com.e.bloodme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.blooddonor);
+
+        Button cancelBtn = (Button) findViewById(R.id.cancelBtn);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), thankyou.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
