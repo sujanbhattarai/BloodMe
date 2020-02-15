3d1b41ed5c4113b38a6cf3dd81a540e1a1abeba5
sujan bhattarai <sujanbhattarai01@gmail.com>
2020-02-14 20:41:48 -0600

    update the sign up java  page

--- app/src/main/java/com/e/bloodme/MainActivity.java
+++ app/src/main/java/com/e/bloodme/MainActivity.java
@@ -24,4 +24,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
+
+    }}
