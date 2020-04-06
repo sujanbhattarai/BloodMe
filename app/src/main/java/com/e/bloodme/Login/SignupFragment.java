1a59898af10af0cca397db7bd1716c40273ffe15
sujan bhattarai <sujanbhattarai01@gmail.com>
2020-04-05 20:17:26 -0500

    ssss

--- app/src/main/java/com/e/bloodme/Login/SignupFragment.java
+++ app/src/main/java/com/e/bloodme/Login/SignupFragment.java
@@ -20,6 +20,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
